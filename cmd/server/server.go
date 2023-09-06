95ffa563005d2547557db1d4e16fe7b59aef43f4
yuriBaza23 <yuribaza2321@gmail.com>
2023-09-06 17:26:00 -0300

    feat: create route

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -3,12 +3,15 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"pi/internal/api/v1/handlers"
 
 	"github.com/gorilla/mux"
 )
 
 func HttpInit(port string) {
 	r := mux.NewRouter()
+
+	r.HandleFunc("/partners", handlers.CreateParner).Methods("POST")
 
 	http.ListenAndServe(fmt.Sprintf(":%s", port), r)
 }
