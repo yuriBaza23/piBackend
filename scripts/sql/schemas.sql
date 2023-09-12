1340c4455029094863e7b77bfb0ce5d114249f6c
Jaovitosr <jv.souzarocha@gmail.com>
2023-09-12 13:31:32 -0300

    update

--- scripts/sql/schemas.sql
+++ scripts/sql/schemas.sql
@@ -1,12 +1,9 @@
--- Uma incubadora possui um ID, nome, senha e email. Seguindo o padrão, possuem datas de criação e atualização
--- Empresas incubadas devem possuir uma foreign key que aponta para o id da incubadora da qual faz parte
+create database pi;
 
-create table if not exists incubators (
-    id uuid default gen_random_uuid() primary key, 
-    name varchar not null,
-    password varchar not null,
-    email varchar not null,
-    createdAt timestamp default now(),
-    updatedAt timestamp default now()
-);
+create user user_pi;
 
+alter user user_pi with encrypted password '1234';
+
+grant all privileges on database pi to user_pi;
+grant all privileges on all tables in schema public to user_pi;
+grant all privileges on all sequences in schema public to user_pi;
