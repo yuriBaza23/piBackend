4971c225ddd774ecca958193a962c68f98a9a4d6
JoãosRocha <75400474+Jaovitosr@users.noreply.github.com>
2023-09-12 13:34:41 -0300

    Update

--- scripts/sql/schemas.sql
+++ scripts/sql/schemas.sql
@@ -1,9 +1,1 @@
-create database pi;
 
-create user user_pi;
-
-alter user user_pi with encrypted password '1234';
-
-grant all privileges on database pi to user_pi;
-grant all privileges on all tables in schema public to user_pi;
-grant all privileges on all sequences in schema public to user_pi;
